ef37a7b5e0b274c2f03712147aa5ada4895ae1db
Rodrigo Honorio <rodrigohono43@outlook.com>
2026-01-16 14:20:13 -0300

    16.1 14.20

--- src/pages/Usuarios.jsx
+++ src/pages/Usuarios.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { db, auth } from "../api/iFrebase";
+import { db, auth } from "../api/Firebase";
 import { initializeApp } from "firebase/app";
 import {
   getAuth,
